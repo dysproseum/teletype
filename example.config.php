6d91a7aaf8757c86f925f886045c9fd99156663b
dysproseum <dysproseum@gmail.com>
2024-06-12 21:12:02 +0000

    Refactor app for websockets

--- example.config.php
+++ example.config.php
@@ -2,10 +2,8 @@
 
 global $conf;
 
-// MySQL database connection.
-$conf['database'] = [
-  'name' => 'teletype',
-  'user' => 'root',
-  'pass' => 'example',
-  'host' => '127.0.0.1',
-];
+// WebSocket server.
+$conf['com1'] = 'wss://example.com:4000/com1/';
+$conf['com2'] = 'wss://example.com:4000/com2/';
+$conf['com3'] = 'wss://example.com:4000/com3/';
+$conf['com4'] = 'wss://example.com:4000/com4/';
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
+<!DOCTYPE html>
 <html>
 <head>
 <title>Teletype | Dysproseum</title>
 <link rel="stylesheet" type="text/css" href="style.css">
-<script type="text/javascript" src="ping.js"></script>
 <script type="text/javascript" src="teletype.js"></script>
 <body>
   <div class="window">
