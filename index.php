9717ec5cf3d92659f82166ec3bd01d19a51ffb1d
dysproseum <dysproseum@gmail.com>
2024-06-18 04:21:03 +0000

    Mobile styling

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 <title>Teletype | Dysproseum</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<link rel='stylesheet' media='only screen and (max-width: 768px)' href='mobile.css' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
 <script type="text/javascript" src="teletype.js"></script>
 <body>
   <div class="window">
